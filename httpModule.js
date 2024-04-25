175c18dbf112ec6b4d016734ba1d239ffae76bf3
tobiloba <tobiosoba777@gmail.com>
2024-04-25 03:39:00 -0700

    Updated

--- httpModule.js
+++ httpModule.js
@@ -9,8 +9,19 @@ const server = http.createServer((req, res) => {
         res.write(JSON.stringify([{course_1: "Mosh javascript course", course_2: "Web Dev simplified react course"}]))
         res.end()
     }
+    if (req.url === '/courses') {
+        res.write("Courses section")
+        res.end();
+    }
+    if (req.url === '/courses/free') {
+        res.write("Free Courses section");
+        res.end();
+    }
+    if (req.url === '/courses/paid') {
+        res.write("Paid Courses section");
+        res.end();
+    }
 })
 
 server.listen(3000);
-
 console.log("server is listening on port 3000");
